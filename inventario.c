7d2fdc3e94891b1903948f298d2f4ca3068739ca
Paloma Fanticelli <fanticelli.dev@gmail.com>
2025-05-16 22:19:25 +0000

    Exibição e comparação de valores. Conclusão do programa.

--- inventario.c
+++ inventario.c
@@ -18,7 +18,22 @@ int main(){
   double valorTotalA;
   double valorTotalB;
 
+  int resultadoA, resultadoB;
+
   //Exibição.
-  printf("Produto %s tem estoque %u e o valor unitário é %f\n", produtoA, estoqueA, valorA);
-  printf("Produto %s tem estoque %u e o valor unitário é %f\n", produtoB, estoqueB, valorB);
+  printf("Produto %s tem estoque %u e o valor unitário é R$ %.2f\n", produtoA, estoqueA, valorA);
+  printf("Produto %s tem estoque %u e o valor unitário é R$ %.2f\n", produtoB, estoqueB, valorB);
+
+  //Comparação estoque.
+  resultadoA = estoqueA > estoqueMinimoA;
+  resultadoB = estoqueB > estoqueMinimoB;
+
+  printf("\nO produto %s tem estoque minimo de %d\n", produtoA, resultadoA);
+  printf("O produto %s tem estoque minimo de %d\n", produtoB, resultadoB);
+
+  //comparação valores.
+  printf("O valor total de A (R$ %.2f) é maior que o valor total de B(R$ %.2f): %d\n", 
+    estoqueA * valorA, //Impressão do valor de A.
+    estoqueB * valorB, //Impressão do valor de B.
+    (estoqueA * valorA) > (estoqueB * valorB)); //Comparação dos dois.
 }
